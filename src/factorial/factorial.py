fb12f6464c3bda0d5919e96d59adba10b9416f84
FacundoPais <163155584+FacundoPais@users.noreply.github.com>
2025-03-18 17:14:22 -0300

    agregando directamente las 3 modificaciones del script factorial

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -20,9 +20,29 @@ def factorial(num):
             num -= 1
         return fact 
 
-if len(sys.argv) == 0:
-   print("Debe informar un número!")
-   sys.exit()
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
+def procesar_rango(inicio, fin):
+    """Calcula y muestra los factoriales en el rango especificado."""
+    for i in range(inicio, fin + 1):
+        print(f"Factorial {i}! es {factorial(i)}")
 
+
+
+if len(sys.argv) < 2:
+    entrada = input("Ingrese un número o un rango (ejemplo: 5, 4-8, -10, 5-): ")
+else:
+    entrada = sys.argv[1]
+
+
+
+if "-" in entrada:
+    partes = entrada.split("-")
+    if entrada.startswith("-"):  # Caso "-10"  de 1 a 10
+        inicio, fin = 1, int(partes[1])
+    elif entrada.endswith("-"):  # Caso "5-"  de 5 a 60
+        inicio, fin = int(partes[0]), 60
+    else:  # Caso "4-8"  de 4 a 8
+        inicio, fin = int(partes[0]), int(partes[1])
+    procesar_rango(inicio, fin)
+else:
+    num = int(entrada)
+    print(f"Factorial {num}! es {factorial(num)}")
